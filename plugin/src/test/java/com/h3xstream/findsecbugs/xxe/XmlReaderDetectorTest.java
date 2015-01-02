cba3799e216b2324b21f8c568c079d762642a68d
h3xstream <h3xstr@gmail.com>
2015-01-02 13:44:08 -0500

    Add test coverage for XmlReader XXE.

---
+++ plugin/src/test/java/com/h3xstream/findsecbugs/xxe/XmlReaderDetectorTest.java
@@ -0,0 +1,59 @@
+/**
+ * Find Security Bugs
+ * Copyright (c) Philippe Arteau, All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.
+ */
+package com.h3xstream.findsecbugs.xxe;
+
+import com.h3xstream.findbugs.test.BaseDetectorTest;
+import com.h3xstream.findbugs.test.EasyBugReporter;
+import org.mockito.Mockito;
+import org.testng.annotations.Test;
+
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
+
+public class XmlReaderDetectorTest extends BaseDetectorTest {
+
+    @Test
+    public void detectXxe() throws Exception {
+        //Locate test code
+        String[] files = {
+                getClassFilePath("testcode/xxe/XmlReaderVulnerable")
+        };
+
+        //Run the analysis
+        EasyBugReporter reporter = spy(new EasyBugReporter());
+        analyze(files, reporter);
+
+        //Assertions
+        verify(reporter).doReportBug(
+                bugDefinition()
+                        .bugType("XXE_XMLREADER")
+                        .inClass("XmlReaderVulnerable").inMethod("receiveXMLStream").atLine(22)
+                        .build()
+        );
+        Mockito.verify(reporter, Mockito.never()).doReportBug(
+                bugDefinition()
+                        .bugType("XXE_SAXPARSER")
+                        .build()
+        );
+        Mockito.verify(reporter, Mockito.never()).doReportBug(
+                bugDefinition()
+                        .bugType("XXE_DOCUMENT")
+                        .build()
+        );
+    }
+}

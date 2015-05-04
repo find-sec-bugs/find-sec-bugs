8fec3798ac1020afbad3253cc098cffea5dbed82
formanek <david4manek@gmail.com>
2015-05-04 23:58:46 +0200

    Refactoring to more readable code (and probably better performance)

--- plugin/src/main/java/com/h3xstream/findsecbugs/crypto/CipherWithNoIntegrityDetector.java
+++ plugin/src/main/java/com/h3xstream/findsecbugs/crypto/CipherWithNoIntegrityDetector.java
@@ -23,14 +23,12 @@ import edu.umd.cs.findbugs.BugReporter;
 import edu.umd.cs.findbugs.OpcodeStack;
 import edu.umd.cs.findbugs.Priorities;
 import edu.umd.cs.findbugs.bcel.OpcodeStackDetector;
+import java.util.regex.Pattern;
 import org.apache.bcel.Constants;
 
-import java.util.Arrays;
-import java.util.List;
-
 /**
- * <p>
  * This detector mark cipher usage that doesn't provide integrity.
+ * <p>
  * The identification will be made base on the mode use.
  * </p>
  * <p>
@@ -53,6 +51,9 @@ import java.util.List;
  *     </ul>
  * </p>
  * Ref: <a href="http://en.wikipedia.org/wiki/Authenticated_encryption">Wikipedia: Authenticated encryption</a>
+ * Ref for the list of potential ciphers:
+ * http://docs.oracle.com/javase/7/docs/technotes/guides/security/StandardNames.html#impl
+ * Note: Not all ECB mode are vulnerable. see RSA/ECB/*
  */
 public class CipherWithNoIntegrityDetector extends OpcodeStackDetector {
 
@@ -60,10 +61,10 @@ public class CipherWithNoIntegrityDetector extends OpcodeStackDetector {
     private static final String PADDING_ORACLE_TYPE = "PADDING_ORACLE";
     private static final String CIPHER_INTEGRITY_TYPE = "CIPHER_INTEGRITY";
 
-    private static final boolean DEBUG = false;
-    private static final List<String> SECURE_CIPHER_MODES = Arrays.asList("CCM","CWC","OCB","EAX","GCM");
+    private static final Pattern AUTHENTICATED_CIPHER_MODES = Pattern.compile(".*/(CCM|CWC|OCB|EAX|GCM)/.*");
+    private static final Pattern INSECURE_ECB_MODES = Pattern.compile("(AES|DES(ede)?)(/ECB/.*)?");
 
-    private BugReporter bugReporter;
+    private final BugReporter bugReporter;
 
     public CipherWithNoIntegrityDetector(BugReporter bugReporter) {
         this.bugReporter = bugReporter;
@@ -71,40 +72,34 @@ public class CipherWithNoIntegrityDetector extends OpcodeStackDetector {
 
     @Override
     public void sawOpcode(int seen) {
-        if (seen == Constants.INVOKESTATIC && getClassConstantOperand().equals("javax/crypto/Cipher") &&
-                getNameConstantOperand().equals("getInstance")) {
-            OpcodeStack.Item item = stack.getStackItem(stack.getStackDepth() - 1); //The first argument is last
-            if (StackUtils.isConstantString(item)) {
-                String cipherValue = (String) item.getConstant();
-                if (DEBUG) System.out.println(cipherValue);
-
-                //Ref for the list of potential ciphers : http://docs.oracle.com/javase/7/docs/technotes/guides/security/StandardNames.html#impl
-                //Note: Not all ECB mode are vulnerable. see RSA/ECB/*
-                if (cipherValue.contains("AES/ECB/") || cipherValue.contains("DES/ECB/") || cipherValue.contains("DESede/ECB/")
-                        || cipherValue.equals("AES") || cipherValue.equals("DES") || cipherValue.equals("DESede")) {
-                    bugReporter.reportBug(new BugInstance(this, ECB_MODE_TYPE, Priorities.HIGH_PRIORITY) //
-                            .addClass(this).addMethod(this).addSourceLine(this));
-                }
-
-                if (cipherValue.contains("/CBC/PKCS5Padding")) {
-                    bugReporter.reportBug(new BugInstance(this, PADDING_ORACLE_TYPE, Priorities.HIGH_PRIORITY) //
-                            .addClass(this).addMethod(this).addSourceLine(this));
-                }
-
-                String[] cipherDefinition = cipherValue.split("/");
-                if(cipherDefinition.length > 1 && !cipherValue.startsWith("RSA/")) { //Some cipher will not have mode specified (ie: "RSA" .. issue GitHub #24)
-                    String cipherMode = cipherDefinition[1];
-
-                    if (!SECURE_CIPHER_MODES.contains(cipherMode)) { //Not a secure mode!
-                        bugReporter.reportBug(new BugInstance(this, CIPHER_INTEGRITY_TYPE, Priorities.HIGH_PRIORITY) //
-                                .addClass(this).addMethod(this).addSourceLine(this));
-                    }
-                }
-                if (cipherValue.equals("AES") || cipherValue.equals("DES") || cipherValue.equals("DESede")) {
-                    bugReporter.reportBug(new BugInstance(this, CIPHER_INTEGRITY_TYPE, Priorities.HIGH_PRIORITY)
-                            .addClass(this).addMethod(this).addSourceLine(this));
-                }
-            }
+        if ((seen != Constants.INVOKESTATIC
+                || !getClassConstantOperand().equals("javax/crypto/Cipher"))
+                || !getNameConstantOperand().equals("getInstance")) {
+            return;
         }
+        OpcodeStack.Item item = stack.getStackItem(stack.getStackDepth() - 1);
+        String cipherValue;
+        if (StackUtils.isConstantString(item)) {
+            cipherValue = (String) item.getConstant();
+        } else {
+            return;
+        }
+        if (INSECURE_ECB_MODES.matcher(cipherValue).matches()) {
+            reportBug(ECB_MODE_TYPE);
+        }
+        if (cipherValue.contains("/CBC/PKCS5Padding")) {
+            reportBug(PADDING_ORACLE_TYPE);
+        }
+
+        //Some cipher will not have mode specified (ie: "RSA" .. issue GitHub #24)
+        if (!AUTHENTICATED_CIPHER_MODES.matcher(cipherValue).matches()
+                && !cipherValue.startsWith("RSA")) {
+            reportBug(CIPHER_INTEGRITY_TYPE);
+        }
+    }
+
+    private void reportBug(String type) {
+        bugReporter.reportBug(new BugInstance(this, type, Priorities.HIGH_PRIORITY)
+                .addClass(this).addMethod(this).addSourceLine(this));
     }
 }
--- plugin/src/test/java/testcode/crypto/CipherNoIntegrity.java
+++ plugin/src/test/java/testcode/crypto/CipherNoIntegrity.java
@@ -7,8 +7,8 @@ public class CipherNoIntegrity {
     public static void main(String[] args) throws Exception {
         Cipher.getInstance("AES/GCM/..."); // ok
         Cipher.getInstance("AES"); // ECB and no integrity
-        Cipher.getInstance("AES/CTR/NoPadding"); // no integrity
-        Cipher.getInstance("AES/ECB/PKCS5Padding"); // ECB and no integrity
+        Cipher.getInstance("DES/CTR/NoPadding"); // no integrity
+        Cipher.getInstance("DESede/ECB/PKCS5Padding"); // ECB and no integrity
         Cipher.getInstance("AES/CBC/PKCS5Padding"); // oracle and no integrity
         Cipher.getInstance("RSA"); // ok
         Cipher.getInstance("RSA/ECB/PKCS1Padding"); // ok

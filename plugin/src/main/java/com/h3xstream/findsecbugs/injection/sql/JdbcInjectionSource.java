116fe3e1d64213b928d448fe2024327b38b651b7
h3xstream <h3xstr@gmail.com>
2015-08-28 17:23:11 -0400

    Coverage for the JDBC API. #73

--- plugin/src/main/java/com/h3xstream/findsecbugs/injection/sql/JdbcInjectionSource.java
+++ plugin/src/main/java/com/h3xstream/findsecbugs/injection/sql/JdbcInjectionSource.java
@@ -22,9 +22,7 @@ import com.h3xstream.findsecbugs.injection.InjectionPoint;
 import com.h3xstream.findsecbugs.injection.InjectionSource;
 import org.apache.bcel.classfile.Constant;
 import org.apache.bcel.classfile.ConstantUtf8;
-import org.apache.bcel.generic.ConstantPoolGen;
-import org.apache.bcel.generic.InstructionHandle;
-import org.apache.bcel.generic.InvokeInstruction;
+import org.apache.bcel.generic.*;
 
 /**
  *
@@ -40,6 +38,145 @@ public class JdbcInjectionSource implements InjectionSource {
 
     @Override
     public InjectionPoint getInjectableParameters(InvokeInstruction ins, ConstantPoolGen cpg, InstructionHandle insHandle) {
+        //ByteCode.printOpCode(ins,cpg);
+
+        String methodName = ins.getMethodName(cpg);
+        String methodSignature = ins.getSignature(cpg);
+        String className = ins.getClassName(cpg);
+
+        if (ins instanceof INVOKEINTERFACE) {
+            /*
+            INVOKESPECIAL org/springframework/jdbc/core/PreparedStatementCreatorFactory.<init> ((Ljava/lang/String;)V)
+            INVOKESPECIAL org/springframework/jdbc/core/PreparedStatementCreatorFactory.<init> ((Ljava/lang/String;[I)V)
+            INVOKESPECIAL org/springframework/jdbc/core/PreparedStatementCreatorFactory.<init> ((Ljava/lang/String;Ljava/util/List;)V)
+            */
+
+            if (className.equals("java.sql.Statement")) {
+
+                //executeQuery signature
+
+                //INVOKEINTERFACE java/sql/Statement.executeQuery ((Ljava/lang/String;)Ljava/sql/ResultSet;)
+                if (methodName.equals("executeQuery") && methodSignature.equals("(Ljava/lang/String;)Ljava/sql/ResultSet;")) {
+                    return new InjectionPoint(new int[]{0}, SQL_INJECTION_TYPE);
+                }
+
+
+                //INVOKEINTERFACE java/sql/Statement.execute ((Ljava/lang/String;)Z)
+                else if (methodName.equals("execute") && methodSignature.equals("(Ljava/lang/String;)Z")) {
+                    return new InjectionPoint(new int[]{0}, SQL_INJECTION_TYPE);
+                }
+                //INVOKEINTERFACE java/sql/Statement.execute ((Ljava/lang/String;I)Z)
+                else if (methodName.equals("execute") && methodSignature.equals("(Ljava/lang/String;I)Z")) {
+                    return new InjectionPoint(new int[]{1}, SQL_INJECTION_TYPE);
+                }
+                //INVOKEINTERFACE java/sql/Statement.execute ((Ljava/lang/String;[I)Z)
+                else if (methodName.equals("execute") && methodSignature.equals("(Ljava/lang/String;[I)Z")) {
+                    return new InjectionPoint(new int[]{1}, SQL_INJECTION_TYPE);
+                }
+                //INVOKEINTERFACE java/sql/Statement.execute ((Ljava/lang/String;[Ljava/lang/String;)Z)
+                else if (methodName.equals("execute") && methodSignature.equals("(Ljava/lang/String;[Ljava/lang/String;)Z")) {
+                    return new InjectionPoint(new int[]{1}, SQL_INJECTION_TYPE);
+                }
+
+                //executeUpdate signatures
+
+                //INVOKEINTERFACE java/sql/Statement.executeUpdate ((Ljava/lang/String;)I)
+                else if (methodName.equals("executeUpdate") && methodSignature.equals("(Ljava/lang/String;)I")) {
+                    return new InjectionPoint(new int[]{0}, SQL_INJECTION_TYPE);
+                }
+                //INVOKEINTERFACE java/sql/Statement.executeUpdate ((Ljava/lang/String;I)I)
+                else if (methodName.equals("executeUpdate") && methodSignature.equals("(Ljava/lang/String;I)I")) {
+                    return new InjectionPoint(new int[]{1}, SQL_INJECTION_TYPE);
+                }
+                //INVOKEINTERFACE java/sql/Statement.executeUpdate ((Ljava/lang/String;[I)I)
+                else if (methodName.equals("executeUpdate") && methodSignature.equals("(Ljava/lang/String;[I)I")) {
+                    return new InjectionPoint(new int[]{1}, SQL_INJECTION_TYPE);
+                }
+                //INVOKEINTERFACE java/sql/Statement.executeUpdate ((Ljava/lang/String;[Ljava/lang/String;)I)
+                else if (methodName.equals("executeUpdate") && methodSignature.equals("(Ljava/lang/String;[Ljava/lang/String;)I")) {
+                    return new InjectionPoint(new int[]{1}, SQL_INJECTION_TYPE);
+                }
+
+                //executeLargeUpdate signatures
+
+                //INVOKEINTERFACE java/sql/Statement.executeLargeUpdate ((Ljava/lang/String;)J)
+                else if (methodName.equals("executeLargeUpdate") && methodSignature.equals("(Ljava/lang/String;)J")) {
+                    return new InjectionPoint(new int[]{0}, SQL_INJECTION_TYPE);
+                }
+                //INVOKEINTERFACE java/sql/Statement.executeLargeUpdate ((Ljava/lang/String;I)J)
+                else if (methodName.equals("executeLargeUpdate") && methodSignature.equals("(Ljava/lang/String;I)J")) {
+                    return new InjectionPoint(new int[]{1}, SQL_INJECTION_TYPE);
+                }
+                //INVOKEINTERFACE java/sql/Statement.executeLargeUpdate ((Ljava/lang/String;[I)J)
+                else if (methodName.equals("executeLargeUpdate") && methodSignature.equals("(Ljava/lang/String;[I)J")) {
+                    return new InjectionPoint(new int[]{1}, SQL_INJECTION_TYPE);
+                }
+                //INVOKEINTERFACE java/sql/Statement.executeLargeUpdate ((Ljava/lang/String;[Ljava/lang/String;)J)
+                else if (methodName.equals("executeLargeUpdate") && methodSignature.equals("(Ljava/lang/String;[Ljava/lang/String;)J")) {
+                    return new InjectionPoint(new int[]{1}, SQL_INJECTION_TYPE);
+                }
+
+                //INVOKEINTERFACE java/sql/Statement.addBatch ((Ljava/lang/String;)V)
+                else if (methodName.equals("addBatch") && methodSignature.equals("(Ljava/lang/String;)V")) {
+                    return new InjectionPoint(new int[]{0}, SQL_INJECTION_TYPE);
+                }
+
+
+            }
+            else if (className.equals("java.sql.Connection")) {
+
+
+                //prepareCall signatures
+
+                //INVOKEINTERFACE java/sql/Connection.prepareCall ((Ljava/lang/String;)Ljava/sql/CallableStatement;)
+                if (methodName.equals("prepareCall") && methodSignature.equals("(Ljava/lang/String;)Ljava/sql/CallableStatement;")) {
+                    return new InjectionPoint(new int[]{0}, SQL_INJECTION_TYPE);
+                }
+                //INVOKEINTERFACE java/sql/Connection.prepareCall ((Ljava/lang/String;II)Ljava/sql/CallableStatement;)
+                else if (methodName.equals("prepareCall") && methodSignature.equals("(Ljava/lang/String;II)Ljava/sql/CallableStatement;")) {
+                    return new InjectionPoint(new int[]{2}, SQL_INJECTION_TYPE);
+                }
+                //INVOKEINTERFACE java/sql/Connection.prepareCall ((Ljava/lang/String;III)Ljava/sql/CallableStatement;)
+                else if (methodName.equals("prepareCall") && methodSignature.equals("(Ljava/lang/String;III)Ljava/sql/CallableStatement;")) {
+                    return new InjectionPoint(new int[]{3}, SQL_INJECTION_TYPE);
+                }
+
+                //prepareStatement signatures
+
+                //INVOKEINTERFACE java/sql/Connection.prepareStatement ((Ljava/lang/String;)Ljava/sql/PreparedStatement;)
+                else if (methodName.equals("prepareStatement") && methodSignature.equals("(Ljava/lang/String;)Ljava/sql/PreparedStatement;")) {
+                    return new InjectionPoint(new int[]{0}, SQL_INJECTION_TYPE);
+                }
+                //INVOKEINTERFACE java/sql/Connection.prepareStatement ((Ljava/lang/String;I)Ljava/sql/PreparedStatement;)
+                else if (methodName.equals("prepareStatement") && methodSignature.equals("(Ljava/lang/String;I)Ljava/sql/PreparedStatement;")) {
+                    return new InjectionPoint(new int[]{1}, SQL_INJECTION_TYPE);
+                }
+                //INVOKEINTERFACE java/sql/Connection.prepareStatement ((Ljava/lang/String;II)Ljava/sql/PreparedStatement;)
+                else if (methodName.equals("prepareStatement") && methodSignature.equals("(Ljava/lang/String;II)Ljava/sql/PreparedStatement;")) {
+                    return new InjectionPoint(new int[]{2}, SQL_INJECTION_TYPE);
+                }
+                //INVOKEINTERFACE java/sql/Connection.prepareStatement ((Ljava/lang/String;III)Ljava/sql/PreparedStatement;)
+                else if (methodName.equals("prepareStatement") && methodSignature.equals("(Ljava/lang/String;III)Ljava/sql/PreparedStatement;")) {
+                    return new InjectionPoint(new int[]{3}, SQL_INJECTION_TYPE);
+                }
+                //INVOKEINTERFACE java/sql/Connection.prepareStatement ((Ljava/lang/String;[I)Ljava/sql/PreparedStatement;)
+                else if (methodName.equals("prepareStatement") && methodSignature.equals("(Ljava/lang/String;[I)Ljava/sql/PreparedStatement;")) {
+                    return new InjectionPoint(new int[]{1}, SQL_INJECTION_TYPE);
+                }
+                //INVOKEINTERFACE java/sql/Connection.prepareStatement ((Ljava/lang/String;[Ljava/lang/String;)Ljava/sql/PreparedStatement;)
+                else if (methodName.equals("prepareStatement") && methodSignature.equals("(Ljava/lang/String;[Ljava/lang/String;)Ljava/sql/PreparedStatement;")) {
+                    return new InjectionPoint(new int[]{1}, SQL_INJECTION_TYPE);
+                }
+
+                //nativeSQL signatures
+
+                //INVOKEINTERFACE java/sql/Connection.nativeSQL ((Ljava/lang/String;)Ljava/lang/String;)
+                else if (methodName.equals("nativeSQL") && methodSignature.equals("(Ljava/lang/String;)Ljava/lang/String;")) {
+                    return new InjectionPoint(new int[]{0}, SQL_INJECTION_TYPE);
+                }
+
+            }
+        }
 
 
         return InjectionPoint.NONE;
---
+++ plugin/src/test/java/com/h3xstream/findsecbugs/injection/sql/JdbcInjectionSourceTest.java
@@ -0,0 +1,183 @@
+/**
+ * Find Security Bugs
+ * Copyright (c) Philippe Arteau, All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.
+ */
+package com.h3xstream.findsecbugs.injection.sql;
+
+import com.h3xstream.findbugs.test.BaseDetectorTest;
+import com.h3xstream.findbugs.test.EasyBugReporter;
+import org.testng.annotations.Test;
+
+import java.util.Arrays;
+
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+public class JdbcInjectionSourceTest extends BaseDetectorTest {
+
+    @Test
+    public void detectBasicJdbcInjection() throws Exception {
+        //Locate test code
+        String[] files = {
+                getClassFilePath("testcode/sqli/Jdbc")
+        };
+
+        //Run the analysis
+        EasyBugReporter reporter = spy(new EasyBugReporter());
+        analyze(files, reporter);
+
+        //Assertion on the 4 basic test
+        for (String unsafeMethod : Arrays.asList("query1", "query2", "query3", "query4")) {
+            verify(reporter).doReportBug(
+                    bugDefinition()
+                            .bugType("SQL_INJECTION_JDBC")
+                            .inClass("Jdbc")
+                            .inMethod(unsafeMethod)
+                            .build()
+            );
+        }
+    }
+
+    @Test
+    public void detectExecuteQuery() throws Exception {
+        //Locate test code
+        String[] files = {
+                getClassFilePath("testcode/sqli/Jdbc")
+        };
+
+        //Run the analysis
+        EasyBugReporter reporter = spy(new EasyBugReporter());
+        analyze(files, reporter);
+
+        //Assertion
+        verify(reporter,times(5)).doReportBug(
+                bugDefinition()
+                        .bugType("SQL_INJECTION_JDBC")
+                        .inClass("Jdbc")
+                        .inMethod("executeQuerySamples")
+                        .build()
+        );
+    }
+
+
+    @Test
+    public void detectExecuteUpdate() throws Exception {
+        //Locate test code
+        String[] files = {
+                getClassFilePath("testcode/sqli/Jdbc")
+        };
+
+        //Run the analysis
+        EasyBugReporter reporter = spy(new EasyBugReporter());
+        analyze(files, reporter);
+
+        //Assertion
+        verify(reporter,times(4)).doReportBug(
+                bugDefinition()
+                        .bugType("SQL_INJECTION_JDBC")
+                        .inClass("Jdbc")
+                        .inMethod("executeUpdateSamples")
+                        .build()
+        );
+    }
+
+    @Test
+    public void detectExecuteExecuteLargeUpdate() throws Exception {
+        //Locate test code
+        String[] files = {
+                getClassFilePath("testcode/sqli/Jdbc")
+        };
+
+        //Run the analysis
+        EasyBugReporter reporter = spy(new EasyBugReporter());
+        analyze(files, reporter);
+
+        //Assertion
+        verify(reporter,times(4)).doReportBug(
+                bugDefinition()
+                        .bugType("SQL_INJECTION_JDBC")
+                        .inClass("Jdbc")
+                        .inMethod("executeExecuteLargeUpdateSamples")
+                        .build()
+        );
+    }
+
+
+    @Test
+    public void detectExecutePrepareCall() throws Exception {
+        //Locate test code
+        String[] files = {
+                getClassFilePath("testcode/sqli/Jdbc")
+        };
+
+        //Run the analysis
+        EasyBugReporter reporter = spy(new EasyBugReporter());
+        analyze(files, reporter);
+
+        //Assertion
+        verify(reporter,times(3)).doReportBug(
+                bugDefinition()
+                        .bugType("SQL_INJECTION_JDBC")
+                        .inClass("Jdbc")
+                        .inMethod("executePrepareCallSamples")
+                        .build()
+        );
+    }
+
+    @Test
+    public void detectPrepareStatement() throws Exception {
+        //Locate test code
+        String[] files = {
+                getClassFilePath("testcode/sqli/Jdbc")
+        };
+
+        //Run the analysis
+        EasyBugReporter reporter = spy(new EasyBugReporter());
+        analyze(files, reporter);
+
+        //Assertion
+        verify(reporter,times(6)).doReportBug(
+                bugDefinition()
+                        .bugType("SQL_INJECTION_JDBC")
+                        .inClass("Jdbc")
+                        .inMethod("prepareStatementSamples")
+                        .build()
+        );
+    }
+
+
+    @Test
+    public void detectNativeSqlAndOther() throws Exception {
+        //Locate test code
+        String[] files = {
+                getClassFilePath("testcode/sqli/Jdbc")
+        };
+
+        //Run the analysis
+        EasyBugReporter reporter = spy(new EasyBugReporter());
+        analyze(files, reporter);
+
+        //Assertion
+        verify(reporter,times(2)).doReportBug(
+                bugDefinition()
+                        .bugType("SQL_INJECTION_JDBC")
+                        .inClass("Jdbc")
+                        .inMethod("otherSamples")
+                        .build()
+        );
+    }
+}

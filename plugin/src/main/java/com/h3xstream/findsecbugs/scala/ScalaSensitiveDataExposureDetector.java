3092bf1933b42c43750f394f7c680710c2d36268
MaxNad <maxime.nadeau@outlook.com>
2016-07-09 12:09:57 -0400

    Sensitive data exposure vulnerable code detector for Scala

---
+++ plugin/src/main/java/com/h3xstream/findsecbugs/scala/ScalaSensitiveDataExposureDetector.java
@@ -0,0 +1,57 @@
+/**
+ * Find Security Bugs
+ * Copyright (c) Philippe Arteau, All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.
+ */
+package com.h3xstream.findsecbugs.scala;
+
+import com.h3xstream.findsecbugs.FindSecBugsGlobalConfig;
+import com.h3xstream.findsecbugs.common.InterfaceUtils;
+import com.h3xstream.findsecbugs.injection.BasicInjectionDetector;
+import com.h3xstream.findsecbugs.taintanalysis.Taint;
+import com.h3xstream.findsecbugs.taintanalysis.TaintDataflow;
+import com.h3xstream.findsecbugs.taintanalysis.TaintFrame;
+import edu.umd.cs.findbugs.*;
+import edu.umd.cs.findbugs.ba.*;
+import edu.umd.cs.findbugs.bcel.BCELUtil;
+import edu.umd.cs.findbugs.classfile.CheckedAnalysisException;
+import edu.umd.cs.findbugs.classfile.Global;
+import edu.umd.cs.findbugs.classfile.MethodDescriptor;
+import org.apache.bcel.classfile.JavaClass;
+import org.apache.bcel.classfile.Method;
+import org.apache.bcel.generic.*;
+
+import java.util.*;
+
+public class ScalaSensitiveDataExposureDetector extends BasicInjectionDetector {
+
+    private static final String SCALA_SENSITIVE_DATA_EXPOSURE_TYPE = "SCALA_SENSITIVE_DATA_EXPOSURE";
+
+    public ScalaSensitiveDataExposureDetector(BugReporter bugReporter) {
+        super(bugReporter);
+        loadConfiguredSinks("sensitive_data_exposure_scala.txt", SCALA_SENSITIVE_DATA_EXPOSURE_TYPE);
+    }
+
+    @Override
+    protected int getPriority(Taint taint) {
+
+        // If this call doesn't contain any sensitive data - There is no reason to report it.
+        if (!taint.hasTag(Taint.Tag.SENSITIVE_DATA)) {
+            return Priorities.IGNORE_PRIORITY;
+        }
+
+         return super.getPriority(taint);
+    }
+}
--- plugin/src/main/java/com/h3xstream/findsecbugs/taintanalysis/TaintFrameModelingVisitor.java
+++ plugin/src/main/java/com/h3xstream/findsecbugs/taintanalysis/TaintFrameModelingVisitor.java
@@ -26,13 +26,9 @@ import edu.umd.cs.findbugs.ba.InvalidBytecodeException;
 import edu.umd.cs.findbugs.ba.generic.GenericSignatureParser;
 import edu.umd.cs.findbugs.classfile.MethodDescriptor;
 import edu.umd.cs.findbugs.util.ClassName;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Set;
+
+import java.util.*;
+
 import org.apache.bcel.Constants;
 import org.apache.bcel.Repository;
 import org.apache.bcel.classfile.JavaClass;

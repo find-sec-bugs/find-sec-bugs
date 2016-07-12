52d823fa6bb5ddb394183c9ff4890c473d7d9de9
MaxNad <maxime.nadeau@outlook.com>
2016-07-12 15:24:20 -0400

    XSS (CWE 79) - vulnerable code detector for Scala - Changed the MVC API detector to remove the code duplication.
    Also added the AbstractInjectionDetector.getPriorityFromTaintFrame method to allow more modular injection based detectors. This will allow to check for a combination of parameters (for instance a tainted object calling a method with a specific value)

--- plugin/src/main/java/com/h3xstream/findsecbugs/scala/XssMvcApiDetector.java
+++ plugin/src/main/java/com/h3xstream/findsecbugs/scala/XssMvcApiDetector.java
@@ -17,78 +17,58 @@
  */
 package com.h3xstream.findsecbugs.scala;
 
-import com.h3xstream.findsecbugs.injection.AbstractTaintDetector;
+import com.h3xstream.findsecbugs.FindSecBugsGlobalConfig;
+import com.h3xstream.findsecbugs.injection.BasicInjectionDetector;
 import com.h3xstream.findsecbugs.taintanalysis.Taint;
 import com.h3xstream.findsecbugs.taintanalysis.TaintFrame;
-import edu.umd.cs.findbugs.BugInstance;
 import edu.umd.cs.findbugs.BugReporter;
 import edu.umd.cs.findbugs.Priorities;
-import edu.umd.cs.findbugs.SourceLineAnnotation;
-import edu.umd.cs.findbugs.ba.ClassContext;
 import edu.umd.cs.findbugs.ba.DataflowAnalysisException;
-import edu.umd.cs.findbugs.util.ClassName;
-import org.apache.bcel.classfile.Method;
-import org.apache.bcel.generic.ConstantPoolGen;
-import org.apache.bcel.generic.InstructionHandle;
-import org.apache.bcel.generic.InvokeInstruction;
 
-public class XssMvcApiDetector extends AbstractTaintDetector {
+public class XssMvcApiDetector extends BasicInjectionDetector {
 
     private static final String SCALA_XSS_MVC_API_TYPE = "SCALA_XSS_MVC_API";
 
     private final String VULNERABLE_CONTENT_TYPE = "text/html";
-    private final String INJECTION_SINK = "play/api/mvc/Result.as(Ljava/lang/String;)Lplay/api/mvc/Result;";
 
     public XssMvcApiDetector(BugReporter bugReporter) {
         super(bugReporter);
+        loadConfiguredSinks("xss-scala-mvc-api.txt", SCALA_XSS_MVC_API_TYPE);
     }
 
     @Override
-    protected void analyzeLocation(ClassContext classContext, Method method, InstructionHandle handle,
-                                   ConstantPoolGen cpg, InvokeInstruction invoke, TaintFrame fact, String currentMethod)
+    protected int getPriorityFromTaintFrame(TaintFrame fact, int offset)
             throws DataflowAnalysisException {
+        Taint mvcResultTaint = fact.getStackValue(offset);
 
-        SourceLineAnnotation sourceLine = SourceLineAnnotation.fromVisitedInstruction(classContext, method, handle);
+        // The MVC Result object was tainted - This could still be safe if the content-type is a safe one
+        if (!mvcResultTaint.isSafe()) {
+            // Get the value of the content-type parameter
+            Taint parameterTaint = fact.getStackValue(0);
 
-        String className = getInstanceClassName(cpg, invoke, fact);
-        String methodName = "." + invoke.getMethodName(cpg) + invoke.getSignature(cpg);
-        String fullMethodName = className.concat(methodName);
-
-        if (fullMethodName.equals(INJECTION_SINK)) {
-            // Get the value of the MVC Object
-            Taint mvcResultTaint = fact.getStackValue(1);
-
-            // The MVC Result object was tainted - This could still be safe if the content-type is a safe one
-            if (!mvcResultTaint.isSafe()) {
-
-                // Get the value of the content-type parameter
-                Taint parameterTaint = fact.getStackValue(0);
-
-                if (!parameterTaint.isSafe()
-                        || parameterTaint.getConstantValue().equals(VULNERABLE_CONTENT_TYPE)) {
-
-                    bugReporter.reportBug(new BugInstance(this, SCALA_XSS_MVC_API_TYPE, Priorities.NORMAL_PRIORITY)
-                            .addClassAndMethod(classContext.getJavaClass(), method).addSourceLine(sourceLine));
-                }
+            if (!parameterTaint.isSafe() || parameterTaint.getConstantValue().equals(VULNERABLE_CONTENT_TYPE)) {
+                return getPriority(mvcResultTaint);
             }
         }
+
+        return Priorities.IGNORE_PRIORITY;
     }
 
-    private static String getInstanceClassName(ConstantPoolGen cpg, InvokeInstruction invoke, TaintFrame frame) {
-        try {
-            int instanceIndex = frame.getNumArgumentsIncludingObjectInstance(invoke, cpg) - 1;
-            if (instanceIndex != -1) {
-                assert instanceIndex < frame.getStackDepth();
-                Taint instanceTaint = frame.getStackValue(instanceIndex);
-                String className = instanceTaint.getRealInstanceClassName();
-                if (className != null) {
-                    return className;
-                }
+    @Override
+    protected int getPriority(Taint taint) {
+        if (!taint.isSafe() && taint.hasTag(Taint.Tag.XSS_SAFE)) {
+            if(FindSecBugsGlobalConfig.getInstance().isReportPotentialXssWrongContext()) {
+                return Priorities.LOW_PRIORITY;
             }
-        } catch (DataflowAnalysisException ex) {
-            assert false : ex.getMessage();
+            else {
+                return Priorities.IGNORE_PRIORITY;
+            }
+        } else if (!taint.isSafe()
+                && (taint.hasTag(Taint.Tag.QUOTE_ENCODED) || taint.hasTag(Taint.Tag.APOSTROPHE_ENCODED))
+                && taint.hasTag(Taint.Tag.LT_ENCODED)) {
+            return Priorities.LOW_PRIORITY;
+        } else {
+            return super.getPriority(taint);
         }
-        String dottedClassName = invoke.getReferenceType(cpg).toString();
-        return ClassName.toSlashedClassName(dottedClassName);
     }
 }

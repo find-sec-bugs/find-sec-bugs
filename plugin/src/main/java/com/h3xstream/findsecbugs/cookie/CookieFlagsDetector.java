14dbfb64321607453eadc0db991888cbf203b098
MaxNad <maxime.nadeau@outlook.com>
2016-07-14 13:07:41 -0400

    Insecure and non HttpOnly cookies (CWE 311 & CWE 16 ) - Code Cleanup

--- plugin/src/main/java/com/h3xstream/findsecbugs/cookie/CookieFlagsDetector.java
+++ plugin/src/main/java/com/h3xstream/findsecbugs/cookie/CookieFlagsDetector.java
@@ -81,37 +81,58 @@ public class CookieFlagsDetector implements Detector {
                 if ("javax.servlet.http.Cookie".equals(invoke.getClassName(cpg)) &&
                         "<init>".equals(invoke.getMethodName(cpg))) {
 
+                    // The following call should push the cookie onto the stack
+                    Instruction cookieStoreInstruction = loc.getHandle().getNext().getInstruction();
+                    if (cookieStoreInstruction instanceof ASTORE) {
 
-                    Instruction stackPushInstruction = loc.getHandle().getNext().getInstruction();
-                    ASTORE storeInstruction = (ASTORE)stackPushInstruction;
+                        // We will use the position of the object on the stack to track the cookie
+                        ASTORE storeInstruction = (ASTORE)cookieStoreInstruction;
 
-                    Location setSecureLocation = getSetSecureLocation(cpg, loc, storeInstruction.getIndex());
-                    if (setSecureLocation == null) {
+                        Location setSecureLocation = getSetSecureLocation(cpg, loc, storeInstruction.getIndex());
+                        if (setSecureLocation == null) {
 
-                        JavaClass javaClass = classContext.getJavaClass();
+                            JavaClass javaClass = classContext.getJavaClass();
 
-                        bugReporter.reportBug(new BugInstance(this, INSECURE_COOKIE_TYPE, Priorities.NORMAL_PRIORITY) //
-                                .addClass(javaClass)
-                                .addMethod(javaClass, m)
-                                .addSourceLine(classContext, m, loc));
-                    }
+                            bugReporter.reportBug(new BugInstance(this, INSECURE_COOKIE_TYPE, Priorities.NORMAL_PRIORITY) //
+                                    .addClass(javaClass)
+                                    .addMethod(javaClass, m)
+                                    .addSourceLine(classContext, m, loc));
+                        }
 
-                    Location setHttpOnlyLocation = getSetHttpOnlyLocation(cpg, loc, storeInstruction.getIndex());
-                    if (setHttpOnlyLocation == null) {
+                        Location setHttpOnlyLocation = getSetHttpOnlyLocation(cpg, loc, storeInstruction.getIndex());
+                        if (setHttpOnlyLocation == null) {
 
-                        JavaClass javaClass = classContext.getJavaClass();
+                            JavaClass javaClass = classContext.getJavaClass();
 
-                        bugReporter.reportBug(new BugInstance(this, HTTPONLY_COOKIE_TYPE, Priorities.NORMAL_PRIORITY) //
-                                .addClass(javaClass)
-                                .addMethod(javaClass, m)
-                                .addSourceLine(classContext, m, loc));
+                            bugReporter.reportBug(new BugInstance(this, HTTPONLY_COOKIE_TYPE, Priorities.NORMAL_PRIORITY) //
+                                    .addClass(javaClass)
+                                    .addMethod(javaClass, m)
+                                    .addSourceLine(classContext, m, loc));
+                        }
                     }
                 }
             }
         }
     }
 
-    private Location getSetSecureLocation(ConstantPoolGen cpg, Location startLocation, int stackLocation) {
+    /**
+     * This method is used to track calls made on a specific object. For instance, this could be used to track if "setHttpOnly(true)"
+     * was executed on a specific cookie object.
+     *
+     * This allows the detector to find interchanged calls like this
+     *
+     * Cookie cookie1 = new Cookie("f", "foo");     <- This cookie is unsafe
+     * Cookie cookie2 = new Cookie("b", "bar");     <- This cookie is safe
+     * cookie1.setHttpOnly(false);
+     * cookie2.setHttpOnly(true);
+     *
+     * @param cpg ConstantPoolGen
+     * @param startLocation The Location of the cookie initialization call.
+     * @param objectStackLocation The index of the cookie on the stack.
+     * @param invokeInstruction The instruction we want to detect.s
+     * @return The location of the invoke instruction provided for the cookie at a specific index on the stack.
+     */
+    private Location getCookieInstructionLocation(ConstantPoolGen cpg, Location startLocation, int objectStackLocation, String invokeInstruction) {
         Location location = startLocation;
         InstructionHandle handle = location.getHandle();
 
@@ -122,18 +143,22 @@ public class CookieFlagsDetector implements Detector {
             handle = handle.getNext();
             Instruction nextInst = handle.getInstruction();
 
+            // We check if the index of the cookie used for this invoke is the same as the one provided
             if (nextInst instanceof ALOAD) {
                 ALOAD loadInst = (ALOAD)nextInst;
                 loadedStackValue = loadInst.getIndex();
             }
 
             if (nextInst instanceof INVOKEVIRTUAL
-                    && loadedStackValue == stackLocation) {
+                    && loadedStackValue == objectStackLocation) {
                 INVOKEVIRTUAL invoke = (INVOKEVIRTUAL) nextInst;
-                if ("javax.servlet.http.Cookie".equals(invoke.getClassName(cpg)) &&
-                        "setSecure".equals(invoke.getMethodName(cpg))) {
+
+                String methodNameWithSignature = invoke.getClassName(cpg) + "." + invoke.getMethodName(cpg);
+
+                if (methodNameWithSignature.equals(invokeInstruction)) {
+
                     Integer val = ByteCode.getConstantInt(handle.getPrev());
-                    //if(val != null) System.out.println("setSecure -> "+val.intValue());
+
                     if (val != null && val == TRUE_INT_VALUE) {
                         return new Location(handle, location.getBasicBlock());
                     }
@@ -144,37 +169,12 @@ public class CookieFlagsDetector implements Detector {
         return null;
     }
 
+    private Location getSetSecureLocation(ConstantPoolGen cpg, Location startLocation, int stackLocation) {
+        return getCookieInstructionLocation(cpg, startLocation, stackLocation, "javax.servlet.http.Cookie.setSecure");
+    }
+
     private Location getSetHttpOnlyLocation(ConstantPoolGen cpg, Location startLocation, int stackLocation) {
-        Location location = startLocation;
-        InstructionHandle handle = location.getHandle();
-
-        int loadedStackValue = 0;
-
-        // Loop until we find the setHttpOnly call for this cookie
-        while (handle.getNext() != null) {
-            handle = handle.getNext();
-            Instruction nextInst = handle.getInstruction();
-
-            if (nextInst instanceof ALOAD) {
-                ALOAD loadInst = (ALOAD)nextInst;
-                loadedStackValue = loadInst.getIndex();
-            }
-
-            if (nextInst instanceof INVOKEVIRTUAL
-                && loadedStackValue == stackLocation) {
-                INVOKEVIRTUAL invoke = (INVOKEVIRTUAL) nextInst;
-                if ("javax.servlet.http.Cookie".equals(invoke.getClassName(cpg)) &&
-                        "setHttpOnly".equals(invoke.getMethodName(cpg))) {
-                    Integer val = ByteCode.getConstantInt(handle.getPrev());
-                    //if(val != null) System.out.println("setSecure -> "+val.intValue());
-                    if (val != null && val == TRUE_INT_VALUE) {
-                        return new Location(handle, location.getBasicBlock());
-                    }
-                }
-            }
-        }
-
-        return null;
+        return getCookieInstructionLocation(cpg, startLocation, stackLocation, "javax.servlet.http.Cookie.setHttpOnly");
     }
 
     @Override

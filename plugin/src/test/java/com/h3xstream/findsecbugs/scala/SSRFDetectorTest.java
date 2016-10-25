dfdb7545135a31e6e1c538ecdcc5f62ab22440d6
MaxNad <maxime.nadeau@outlook.com>
2016-10-24 21:24:41 -0400

    Server Side Request Forgery (CWE 918) - Create a new detector for basic server redirection test cases

---
+++ plugin/src/test/java/com/h3xstream/findsecbugs/scala/SSRFDetectorTest.java
@@ -0,0 +1,89 @@
+/**
+ * Find Security Bugs
+ * Copyright (c) Philippe Arteau, All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.
+ */
+package com.h3xstream.findsecbugs.scala;
+
+import com.h3xstream.findbugs.test.BaseDetectorTest;
+import com.h3xstream.findbugs.test.EasyBugReporter;
+import org.testng.annotations.Test;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
+
+public class SSRFDetectorTest extends BaseDetectorTest {
+
+    @Test
+    public void detectXssInController() throws Exception {
+        //FindSecBugsGlobalConfig.getInstance().setDebugPrintInstructionVisited(true);
+        //FindSecBugsGlobalConfig.getInstance().setDebugPrintInvocationVisited(false);
+        //FindSecBugsGlobalConfig.getInstance().setDebugTaintState(true);
+
+        //Locate test code
+        String[] files = {
+                getClassFilePath("bytecode_samples/scala_play_ssrf.jar")
+        };
+
+        //Run the analysis
+        EasyBugReporter reporter = spy(new BaseDetectorTest.SecurityReporter());
+        analyze(files, reporter);
+
+        // Test the MVC API checks
+        verify(reporter).doReportBug(
+                bugDefinition()
+                        .bugType("SCALA_PLAY_SSRF")
+                        .inClass("SSRFController").inMethod("vulnerableGet").atLine(20)
+                        .build()
+        );
+
+        verify(reporter).doReportBug(
+                bugDefinition()
+                        .bugType("SCALA_PLAY_SSRF")
+                        .inClass("SSRFController").inMethod("vulnerablePost").atLine(29)
+                        .build()
+        );
+
+        verify(reporter).doReportBug(
+                bugDefinition()
+                        .bugType("SCALA_PLAY_SSRF")
+                        .inClass("SSRFController").inMethod("vulnerablePost").atLine(33)
+                        .build()
+        );
+
+        // Test the Twirl template engine checks
+        verify(reporter).doReportBug(
+                bugDefinition()
+                        .bugType("SCALA_PLAY_SSRF")
+                        .inClass("SSRFController").inMethod("vulnerablePost").atLine(38)
+                        .build()
+        );
+
+        //Assertions for safe calls and false positives
+
+        verify(reporter, never()).doReportBug(
+                bugDefinition()
+                        .bugType("SCALA_XSS_MVC_API")
+                        .inClass("SSRFController").inMethod("safeGetNotTainted")
+                        .build()
+        );
+        verify(reporter, never()).doReportBug(
+                bugDefinition()
+                        .bugType("SCALA_XSS_TWIRL")
+                        .inClass("SSRFController").inMethod("safePostNotTainted")
+                        .build()
+        );
+    }
+}

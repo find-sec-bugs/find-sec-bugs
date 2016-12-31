77bc76e9db03c8a4da42314661a0072fd08565c8
plr0man <plr0man@users.noreply.github.com>
2016-12-30 20:56:37 -0600

    Adding Anonymous LDAP Bind tests

---
+++ plugin/src/test/java/com/h3xstream/findsecbugs/AnonymousLdapDetectorTest.java
@@ -0,0 +1,53 @@
+/**
+ * Find Security Bugs
+ * Copyright (c) Philippe Arteau, All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.
+ */
+package com.h3xstream.findsecbugs;
+
+import com.h3xstream.findbugs.test.BaseDetectorTest;
+import com.h3xstream.findbugs.test.EasyBugReporter;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import org.testng.annotations.Test;
+
+public class AnonymousLdapDetectorTest extends BaseDetectorTest {
+
+    @Test
+    public void detectAnonymousLdapBind() throws Exception {
+        //Locate test code
+        String[] files = {
+                getClassFilePath("testcode/AnonymousLdap")
+        };
+
+        //Run the analysis
+        EasyBugReporter reporter = spy(new SecurityReporter());
+        analyze(files, reporter);
+
+        verify(reporter).doReportBug(
+                bugDefinition()
+                        .bugType("LDAP_ANONYMOUS")
+                        .inClass("AnonymousLdap").inMethod("ldapContext").atLine(16)
+                        .build()
+        );
+
+        verify(reporter, times(1)).doReportBug(
+                bugDefinition()
+                        .bugType("LDAP_ANONYMOUS")
+                        .build()
+        );
+    }
+}

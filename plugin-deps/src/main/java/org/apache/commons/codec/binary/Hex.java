f0960905b1e4198130717c3c987a04d4801817a4
Philippe Arteau <parteau@gosecure.ca>
2017-08-07 15:30:55 -0400

    Stub interface for Apache Commons Codec Hex #291

--- plugin-deps/src/main/java/org/apache/commons/codec/binary/Hex.java
+++ plugin-deps/src/main/java/org/apache/commons/codec/binary/Hex.java
@@ -1,7 +1,48 @@
 package org.apache.commons.codec.binary;
 
-/**
- * Created by parteau on 8/7/2017.
- */
+import java.nio.ByteBuffer;
+
 public class Hex {
+
+    public static char[] encodeHex(final byte[] data) {
+        return null;
+    }
+
+    public static char[] encodeHex(final ByteBuffer data) {
+        return null;
+    }
+
+    public static char[] encodeHex(final byte[] data, final boolean toLowerCase) {
+        return null;
+    }
+
+    public static char[] encodeHex(final ByteBuffer data, final boolean toLowerCase) {
+        return null;
+    }
+
+    public static String encodeHexString(final byte[] data) {
+        return null;
+    }
+
+    public static String encodeHexString(final byte[] data, boolean toLowerCase) {
+        return null;
+    }
+
+    public static String encodeHexString(final ByteBuffer data) {
+        return null;
+    }
+
+    public static String encodeHexString(final ByteBuffer data, boolean toLowerCase) {
+        return null;
+    }
+
+    public byte[] encode(final byte[] array) {
+        return null;
+    }
+
+    public byte[] encode(final ByteBuffer array) {
+        return null;
+    }
+
+
 }

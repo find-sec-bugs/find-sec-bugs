e538ab2be9d1aa7c2395a6e5942437686a715473
Brad Flood <bflood@keyholesoftware.com>
2017-11-04 13:56:36 -0500

    https://github.com/find-sec-bugs/find-sec-bugs/issues/341
    identify XSS in ServletOutputStream.print and println

--- plugin/src/test/java/testcode/xss/servlets/XssServlet1.java
+++ plugin/src/test/java/testcode/xss/servlets/XssServlet1.java
@@ -1,22 +1,25 @@
 package testcode.xss.servlets;
 
-import org.apache.commons.lang.StringEscapeUtils;
-import org.owasp.esapi.ESAPI;
-
+import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import org.apache.commons.lang.StringEscapeUtils;
+import org.owasp.esapi.ESAPI;
 
 public class XssServlet1 extends HttpServlet {
 
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String input1 = req.getParameter("input1");
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    String input1 = req.getParameter("input1");
 
-        resp.getWriter().write(input1);
+    resp.getWriter().write(input1);
 
-        resp.getWriter().write(ESAPI.encoder().encodeForHTML(input1));
-        resp.getWriter().write(StringEscapeUtils.escapeHtml(input1));
-    }
+    resp.getWriter().write(ESAPI.encoder().encodeForHTML(input1));
+    resp.getWriter().write(StringEscapeUtils.escapeHtml(input1));
+
+    resp.getOutputStream().print(input1);
+    resp.getOutputStream().println(input1);
+  }
 }

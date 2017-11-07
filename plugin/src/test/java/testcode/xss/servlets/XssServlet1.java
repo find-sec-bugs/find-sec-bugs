139763e1a36bbd1b24f97cde6388bfe96e975643
Brad Flood <bflood@keyholesoftware.com>
2017-11-06 21:06:54 -0600

    formatting changes

--- plugin/src/test/java/testcode/xss/servlets/XssServlet1.java
+++ plugin/src/test/java/testcode/xss/servlets/XssServlet1.java
@@ -10,16 +10,16 @@ import org.owasp.esapi.ESAPI;
 
 public class XssServlet1 extends HttpServlet {
 
-  @Override
-  protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-    String input1 = req.getParameter("input1");
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String input1 = req.getParameter("input1");
 
-    resp.getWriter().write(input1);
+        resp.getWriter().write(input1);
 
-    resp.getWriter().write(ESAPI.encoder().encodeForHTML(input1));
-    resp.getWriter().write(StringEscapeUtils.escapeHtml(input1));
+        resp.getWriter().write(ESAPI.encoder().encodeForHTML(input1));
+        resp.getWriter().write(StringEscapeUtils.escapeHtml(input1));
 
-    resp.getOutputStream().print(input1);
-    resp.getOutputStream().println(input1);
-  }
+        resp.getOutputStream().print(input1);
+        resp.getOutputStream().println(input1);
+    }
 }

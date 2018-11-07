2e399718cd7bd9fda2e28e7deffaecee67395a2f
Jean-Baptiste Le Duigou <jb.leduigou@gmail.com>
2018-11-07 19:36:38 +0100

    Update to use findsecbugs-plugin 1.8.0

--- cli/findsecbugs.sh
+++ cli/findsecbugs.sh
@@ -1,4 +1,4 @@
 
 SOURCE="${BASH_SOURCE[0]}"
 
-java -cp "$SOURCE/lib/\*:" edu.umd.cs.findbugs.LaunchAppropriateUI -quiet -pluginList lib/findsecbugs-plugin-1.7.1.jar -include include.xml $@
+java -cp "$SOURCE/lib/\*:" edu.umd.cs.findbugs.LaunchAppropriateUI -quiet -pluginList lib/findsecbugs-plugin-1.8.0.jar -include include.xml $@

3d5601a4cee9ada763dd3605b1a4882c7c248aaa
Philippe Arteau <parteau@gosecure.ca>
2019-04-02 12:56:34 -0400

    Move the Java samples to a dedicated sub-module.

--- findsecbugs-samples-deps/src/main/java/org/apache/commons/lang/StringEscapeUtils.java
+++ findsecbugs-samples-deps/src/main/java/org/apache/commons/lang/StringEscapeUtils.java
@@ -1,46 +1,57 @@
-//Stub class part of the dependencies already.. Uncommented when necessary
+package org.apache.commons.lang;
 
-//package org.apache.commons.lang;
-//
-//public class StringEscapeUtils {
-//
-//    public static final String escapeCsv(final String input) {
-//        return null;
-//    }
-//
-//    public static final String unescapeCsv(final String input) {
-//        return null;
-//    }
-//
-//    public static final String escapeHtml(final String input) {
-//        return null;
-//    }
-//
-//    public static final String unescapeHtml(final String input) {
-//        return null;
-//    }
-//
-//    public static final String escapeJava(final String input) {
-//        return null;
-//    }
-//
-//    public static final String unescapeJava(final String input) {
-//        return null;
-//    }
-//
-//    public static final String escapeJavaScript(final String input) {
-//        return null;
-//    }
-//
-//    public static final String unescapeJavaScript(final String input) {
-//        return null;
-//    }
-//
-//    public static final String escapeSql(final String input) {
-//        return null;
-//    }
-//
-//    public static final String escapeXml(final String input) {
-//        return null;
-//    }
-//}
+import java.io.PrintWriter;
+import java.io.Writer;
+
+public class StringEscapeUtils {
+
+    public static final String escapeCsv(final String input) {
+        return null;
+    }
+
+    public static final String unescapeCsv(final String input) {
+        return null;
+    }
+
+    public static final String escapeHtml(final String input) {
+        return null;
+    }
+
+    public static final String unescapeHtml(final String input) {
+        return null;
+    }
+
+    public static final String escapeJava(final String input) {
+        return null;
+    }
+
+    public static final String unescapeJava(final String input) {
+        return null;
+    }
+
+    public static final String escapeJavaScript(final String input) {
+        return null;
+    }
+
+    public static final String unescapeJavaScript(final String input) {
+        return null;
+    }
+
+    public static final String escapeSql(final String input) {
+        return null;
+    }
+
+    public static final String escapeXml(final String input) {
+        return null;
+    }
+
+    public static void escapeHtml(Writer writer, String input1) {
+
+    }
+    public static void escapeJavaScript(Writer writer, String input1) {
+
+    }
+    public static void escapeXml(Writer writer, String input1) {
+
+    }
+}

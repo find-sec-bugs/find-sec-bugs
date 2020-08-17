79718c85fd5d8b0bdc43301d31c8673472808bbd
Philippe Arteau <parteau@gosecure.ca>
2020-08-17 01:14:36 -0400

    Hardcoded password with JSch API #536

---
+++ findsecbugs-plugin/src/test/java/com/h3xstream/findsecbugs/password/JschPasswordDetectorTest.java
@@ -0,0 +1,59 @@
+/**
+ * Find Security Bugs
+ * Copyright (c) Philippe Arteau, All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.
+ */
+package com.h3xstream.findsecbugs.password;
+
+import com.h3xstream.findbugs.test.BaseDetectorTest;
+import com.h3xstream.findbugs.test.EasyBugReporter;
+import com.h3xstream.findsecbugs.FindSecBugsGlobalConfig;
+import org.testng.annotations.Test;
+
+import java.util.Arrays;
+
+import static org.mockito.Mockito.*;
+
+public class JschPasswordDetectorTest extends BaseDetectorTest{
+
+
+    @Test
+    public void detectHardCodeSshPasswords() throws Exception {
+        FindSecBugsGlobalConfig.getInstance().setDebugPrintInvocationVisited(true);
+//        FindSecBugsGlobalConfig.getInstance().setDebugTaintState(false);
+
+
+        String[] files = {
+                getClassFilePath("testcode/password/JschSshPassword"),
+        };
+
+        EasyBugReporter reporter = spy(new BaseDetectorTest.SecurityReporter());
+        analyze(files, reporter);
+
+        verify(reporter,times(4)).doReportBug(
+                bugDefinition()
+                        .bugType("HARD_CODE_PASSWORD")
+                        .inClass("JschSshPassword").inMethod("hashcodedPassword")
+                        .build()
+        );
+
+        verify(reporter,never()).doReportBug(
+                bugDefinition()
+                        .bugType("HARD_CODE_PASSWORD")
+                        .inClass("JschSshPassword").inMethod("unknownSourcePassword")
+                        .build()
+        );
+    }
+}

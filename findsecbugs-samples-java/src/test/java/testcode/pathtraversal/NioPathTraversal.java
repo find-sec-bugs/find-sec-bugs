ccb633f30f39046f2c5172854761fcff9576cb28
deepsan <159417+deepsan@users.noreply.github.com>
2021-07-28 11:43:52 -0700

    Add Paths.get(Uri) as source for Path traversal
    This adds the Paths.get(Uri) variant as a follow-up to https://github.com/find-sec-bugs/find-sec-bugs/issues/324
    > From the Path instance built, it can be use to load the file using other API (File, FileInputStream, etc..).
    
    This also brings the nio detector in parity with the scala version of the same detector(scala-path-traversal-in.txt) which handles Uri

--- findsecbugs-samples-java/src/test/java/testcode/pathtraversal/NioPathTraversal.java
+++ findsecbugs-samples-java/src/test/java/testcode/pathtraversal/NioPathTraversal.java
@@ -1,19 +1,26 @@
 package testcode.pathtraversal;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.nio.file.Paths;
 
 public class NioPathTraversal {
 
-    public void loadFile(String path) {
+    public void loadFile(String path) throws URISyntaxException {
         Paths.get(path);
         Paths.get(path,"foo");
         Paths.get(path,"foo", "bar");
         Paths.get("foo", path);
         Paths.get("foo", "bar", path);
 
+        Paths.get(new URI("file", path, null));
+        Paths.get(new URI("file", path, "foo"));
+
         Paths.get("foo");
         Paths.get("foo","bar");
         Paths.get("foo","bar", "allsafe");
+        Paths.get(new URI("file", "foo", null));
+        Paths.get(new URI("file", "foo", "bar"));
 
     }
 }

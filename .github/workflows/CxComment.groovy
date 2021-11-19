7252992505937146c9083adfa3d9de33060bc447
nirw <nirw@users.noreply.github.com>
2021-11-19 22:11:41 +1100

    Delete CxComment.groovy

--- .github/workflows/CxComment.groovy
+++
@@ -1,9 +0,0 @@
-@Grab(group='org.codehaus.groovy.modules.http-builder', module='http-builder', version='0.7' )
-import groovyx.net.http.HTTPBuilder
-def branch = request.getBranch()
-def hash = request.getHash()
-def repoUrl = request.getRepoUrl()
-String comment = "$branch | $hash"
-println "" + request.toString()
-println "INFO : Repo URL: $repoUrl | Branch: $branch | Commit Hash: $hash"
-return comment

a4065e1d07169f3ed2a1e45361d9d695710a326a
Philippe Arteau <philippe.arteau@gmail.com>
2022-01-27 01:24:51 -0500

    Better comments

--- findsecbugs-plugin/src/main/java/com/h3xstream/findsecbugs/injection/xml/NewXmlInjectionDetector.java
+++ findsecbugs-plugin/src/main/java/com/h3xstream/findsecbugs/injection/xml/NewXmlInjectionDetector.java
@@ -1,3 +1,20 @@
+/**
+ * Find Security Bugs
+ * Copyright (c) Philippe Arteau, All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.
+ */
 package com.h3xstream.findsecbugs.injection.xml;
 
 import com.h3xstream.findsecbugs.common.matcher.InvokeMatcherBuilder;
@@ -20,11 +37,12 @@ import static com.h3xstream.findsecbugs.common.matcher.InstructionDSL.invokeInst
 
 /**
  * Detect string concatenation that appears to be constructing XML or HTML documents.
+ *
+ * @author baloghadamsoftware, h3xstream
  */
 public class NewXmlInjectionDetector extends BasicInjectionDetector implements TaintFrameAdditionalVisitor {
 
     private static final String XML_INJECTION_TYPE = "POTENTIAL_XML_INJECTION";
-
     private static final String[] STRING_CONCAT_CLASS = new String[] {"java/lang/StringBuilder","java/lang/StringBuffer"};
 
     private static final InvokeMatcherBuilder STRINGBUILDER_APPEND = invokeInstruction() //
@@ -35,8 +53,7 @@ public class NewXmlInjectionDetector extends BasicInjectionDetector implements T
     public NewXmlInjectionDetector(BugReporter bugReporter) {
         super(bugReporter);
 
-        for(String variant: STRING_CONCAT_CLASS) {
-
+        for(String variant: STRING_CONCAT_CLASS) { //Small hack to avoid loading a signature files for two functions.
             addParsedInjectionPoint(variant+".append(Ljava/lang/String;)L"+variant+";",new InjectionPoint(new int[] {0}, XML_INJECTION_TYPE));
         }
         registerVisitor(this);
@@ -47,7 +64,12 @@ public class NewXmlInjectionDetector extends BasicInjectionDetector implements T
         Taint taint0 = taintFrame.getStackValue(0);
         Taint taint1 = taintFrame.getStackValue(1);
 
-        if ((!taint0.isSafe() && !taint0.hasTag(Taint.Tag.XSS_SAFE)) && taint1.hasTag(Taint.Tag.XML_VALUE)) {
+        /**
+         * If the value argument passed to append is unsafe (not constant)[1] and not sanitize for XML (XSS_SAFE) [2]
+         * and the StringBuilder to which the dynamic value is added is within XML tags [3]
+         * [1] && [2] && [3]
+         */
+        if (!taint0.isSafe() && !taint0.hasTag(Taint.Tag.XSS_SAFE) && taint1.hasTag(Taint.Tag.XML_VALUE)) {
             return Priorities.NORMAL_PRIORITY;
         }
         else {
@@ -68,14 +90,13 @@ public class NewXmlInjectionDetector extends BasicInjectionDetector implements T
     public void visitInvoke(InvokeInstruction invoke, MethodGen methodGen, TaintFrame frameType, List<Taint> parameters, ConstantPoolGen cpg) throws DataflowAnalysisException {
 
         if(STRINGBUILDER_APPEND.matches(invoke,cpg)) {
-//            System.out.println("c="+invoke.getClassName(cpg));
-//            System.out.println("m="+invoke.getMethodName(cpg));
-//            System.out.println(frameType.toString(methodGen));
 
             Taint appendedTaint = parameters.get(0);
             String appendedString = appendedTaint.getConstantValue();
             if(appendedString!= null) {
 
+                //When the string with a tag is sent to the append function.
+                //The StringBuilder class will be tagged as XML_VALUE
                 if(appendedString.contains("<") && appendedString.contains(">")) {
                     appendedTaint.addTag(Taint.Tag.XML_VALUE);
                     frameType.getStackValue(0).addTag(Taint.Tag.XML_VALUE);
@@ -102,7 +123,15 @@ public class NewXmlInjectionDetector extends BasicInjectionDetector implements T
 
     }
 
-
+    /**
+     * Before we added new tag to the taint analysis and add more effort, here is a linear search on the constant pool.
+     * Constant pool include all the constant use in the code of the class. It contains class references and string value.
+     *
+     * If there are no XML in string in the class, we are add not going to run this additional visitor.
+     *
+     * @param classContext Information about the class that is about to be analyzed
+     * @return
+     */
     @Override
     public boolean shouldAnalyzeClass(ClassContext classContext) {
         ConstantPoolGen constantPoolGen = classContext.getConstantPoolGen();

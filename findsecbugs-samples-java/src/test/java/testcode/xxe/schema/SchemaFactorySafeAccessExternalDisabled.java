9e8a48eb6b144d6194f9f5f3809b80d06d5a6f6d
Tomáš Polešovský <tomas.polesovsky@liferay.com>
2025-03-12 16:59:20 +0100

    Change SchemaFactoryDetector to InjectionDetector to use taint data flow. Apply custom tags to SchemaFactory taint to signal when the processing is safe.
    Fixes #738

--- findsecbugs-samples-java/src/test/java/testcode/xxe/schema/SchemaFactorySafeAccessExternalDisabled.java
+++ findsecbugs-samples-java/src/test/java/testcode/xxe/schema/SchemaFactorySafeAccessExternalDisabled.java
@@ -1,12 +1,18 @@
 package testcode.xxe.schema;
 
 import org.xml.sax.SAXException;
+import org.xml.sax.SAXNotRecognizedException;
+import org.xml.sax.SAXNotSupportedException;
 
 import javax.xml.XMLConstants;
 import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.Schema;
 import javax.xml.validation.SchemaFactory;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
 import java.nio.charset.StandardCharsets;
 
 public class SchemaFactorySafeAccessExternalDisabled {
@@ -23,5 +29,37 @@ public class SchemaFactorySafeAccessExternalDisabled {
         final StreamSource source = new StreamSource(inputStream);
 
         schemaFactory.newSchema(source);
+    }
+
+    public static Schema createSchemaAndConfigureInMethod() throws SAXException,
+            MalformedURLException {
+
+        SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+        safelyConfigureSchemaFactoryInMethod(schemaFactory);
+
+        URL schemaFile = URI.create("some-file.xsd").toURL();
+        return schemaFactory.newSchema(schemaFile);
+    }
+
+    private static void safelyConfigureSchemaFactoryInMethod(SchemaFactory schemaFactory) throws SAXNotSupportedException,
+            SAXNotRecognizedException {
+        schemaFactory.setProperty(XMLConstants.ACCESS_EXTERNAL_DTD, EXTERNAL_PROTOCOLS_DISABLED);
+        schemaFactory.setProperty(XMLConstants.ACCESS_EXTERNAL_SCHEMA, EXTERNAL_PROTOCOLS_DISABLED);
+    }
+
+    public static Schema createAncConfigureSchemaInMethod() throws SAXException,
+            MalformedURLException {
+        SchemaFactory schemaFactory = safelyCreateAndConfigureSchemaFactoryInMethod();
+
+        URL schemaFile = URI.create("some-file.xsd").toURL();
+        return schemaFactory.newSchema(schemaFile);
+    }
+
+    private static SchemaFactory safelyCreateAndConfigureSchemaFactoryInMethod() throws SAXNotSupportedException,
+            SAXNotRecognizedException {
+        SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+        schemaFactory.setProperty(XMLConstants.ACCESS_EXTERNAL_DTD, EXTERNAL_PROTOCOLS_DISABLED);
+        schemaFactory.setProperty(XMLConstants.ACCESS_EXTERNAL_SCHEMA, EXTERNAL_PROTOCOLS_DISABLED);
+        return schemaFactory;
     }
 }
